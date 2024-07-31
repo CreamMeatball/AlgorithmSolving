f9ea2c76c78abf556ecf253e3c1f07db7de4ea1b
CreamMeatball <pch5445@gmail.com>
2024-07-31 21:08:05 +0900

    1010 solved

--- 1010.py
+++ 1010.py
@@ -1,3 +1,7 @@
+# 조합 문제.
+# combination 함수와 factorial 함수로 해결. 
+# factorial 함수 작성 시 재귀형으로 쓰면 한계 초과로 에러남. 
+
 testCaseNum = int(input())
 
 N = []
@@ -8,5 +12,24 @@ for i in range(testCaseNum):
     N.append(n)
     M.append(m)
     
+result = []
+
+def combination(n, r):
+    if n == r:
+        return 1
+    return int(factorial(n) / (factorial(r) * factorial(n-r)))
+    
+    
+def factorial(n):
+    if n == 1:
+        return 1
+    for i in range(1, n):
+        n *= i
+    return n
+
 for i in range(testCaseNum):
-    print(N[i], M[i])
+    result.append(combination(M[i], N[i]))
+    
+for i in range(testCaseNum):
+    print(result[i])
+    
