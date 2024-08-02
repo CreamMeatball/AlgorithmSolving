8495d6bcfce6845a65da36627818e2ae5552a0cd
CreamMeatball <pch5445@gmail.com>
2024-08-02 14:10:29 +0900

    9498 solved

---
+++ 9498.py
@@ -0,0 +1,3 @@
+testRecord = int(input())
+
+print("A") if (testRecord >= 90) else print("B") if (testRecord >= 80) else print("C") if (testRecord >= 70) else print("D") if (testRecord >= 60) else print("F")
