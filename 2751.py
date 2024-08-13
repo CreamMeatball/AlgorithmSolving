5431185b44e6858f9895eb346f054f510e028e13
CreamMeatball <pch5445@gmail.com>
2024-08-13 15:19:10 +0900

    2751 real solved

--- 2751.py
+++ 2751.py
@@ -1,8 +1,12 @@
+import sys
+
 N = int(input())
 
 numbers = []
 for _ in range(N):
-    numbers.append(int(input()))
+    # numbers.append(int(input()))
+    # N이 100만 이하이므로, input() 대신 sys.stdin.readline() 사용해야만 시간 초과가 안 남.
+    numbers.append(int(sys.stdin.readline().rstrip()))
     
 sorted_numbers = sorted(numbers)
 
