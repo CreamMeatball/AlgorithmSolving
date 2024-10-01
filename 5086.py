fbf08b67810994e559561b411447a2c56282eb19
CreamMeatball <pch5445@gmail.com>
2024-10-01 14:59:15 +0900

    5085 solved

---
+++ 5086.py
@@ -0,0 +1,13 @@
+while(True):
+    a, b = map(int, input().split())
+    if a == b == 0:
+        break
+    if a > b:
+        if a % b == 0:
+            print("multiple")
+        else:
+            print("neither")
+    elif b % a == 0:
+        print("factor")
+    else:
+        print("neither")
