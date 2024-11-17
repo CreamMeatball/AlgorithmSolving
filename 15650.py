ae7fe38eea5f507745b5567cac23b97bc023d6c4
Cream_Meatball <80515579+CreamMeatball@users.noreply.github.com>
2024-11-17 18:15:11 +0900

    15650 solved

---
+++ 15650.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+N, M = map(int, input().split())
+
+numbers = [i for i in range(1, N + 1)]
+
+def dfs(depth, numbers, prev, result):
+    if depth == M:
+        print(' '.join(map(str, result)))
+        return
+    
+    for number in numbers:
+        if number <= prev:
+            continue
+        indexOfNumber = numbers.index(number)
+        result.append(number)
+        numbers.remove(numbers[indexOfNumber])
+        # 한 앞 숫자에 대해, 뒷 숫자 모두 탐색
+        dfs(depth + 1, numbers, number, result)
+        # 이전 단계로 돌아가는 역할
+        result.pop()
+        numbers.insert(indexOfNumber, number)
+        
+        
+result = deque()
+dfs(0, numbers, 0, result)
