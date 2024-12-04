167c81753cdc160d9ef99ea25c7ebd59e7329282
CreamMeatball <pch5445@gmail.com>
2024-12-04 19:58:09 +0900

    1932 typo edit

--- 1932.py
+++ 1932.py
@@ -14,7 +14,7 @@ for i in range(1, n):
     for j in range(i+1):
         if j == 0: # 각 줄에서 맨 앞 숫자일 때
             dp[i][j] = dp[i-1][j] + triangle[i][j]
-        elif j == i: # 가구 줄에서 맨 뒤 숫자일 때
+        elif j == i: # 각 줄에서 맨 뒤 숫자일 때
             dp[i][j] = dp[i-1][j-1] + triangle[i][j]
         else: # 나머지의 경우
             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
