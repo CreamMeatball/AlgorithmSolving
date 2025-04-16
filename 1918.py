da3742329701c9c28029f67befcba0c472585705
CreamMeatball <pch5445@gmail.com>
2025-04-16 18:29:17 +0900

    1918 solving

---
+++ 1918.py
@@ -0,0 +1,15 @@
+equation = list(map(str, input().split()))
+
+class Node(self, ):
+    def __init__(self):
+        self.value
+        self.leftchild
+        self.rightchild
+        
+    def forwardleft(self):
+        contents 
+        
+    def forwardright(self):
+        contents 
+        
+        
