c45f60c6345f2d71e2781765b94f2bc0dbb9bd3b
CreamMeatball <pch5445@gmail.com>
2025-05-06 01:18:20 +0900

    1406 comment edited

--- 1406.py
+++ 1406.py
@@ -17,10 +17,10 @@ for _ in range(M):
     if command[0] == 'L':
         if left_stack:  # 커서가 맨 앞이 아니면
             right_stack.append(left_stack.pop())
-            # right 의 왼쪽에 삽입하는 게 아니라
-            # 그냥 오른쪽에 append 해놨다가
-            # 나중에 반대로 꺼내는 식으로 (stack 방식)
-            # 결국 list의 append, pop의 입출 방향을 반대로 쓰는 것과 같은 효과라고 볼 수도 있음 (deque의 popleft, appendleft 같은 효과)
+            # right stack의 왼쪽으로 삽입하는 게 아니라
+            # 그냥 오른쪽으로 append 및 pop으로 진행하고,
+            # 나중에 꺼낼 때 반대로 꺼내는 식으로.
+            # 왼쪽이 입출구인 stack을 오른쪽이 입출구인 stack이 되는 효과라고 볼 수도 있음 (deque의 popleft, appendleft 가 되는 것 같은 효과)
     
     elif command[0] == 'D':
         if right_stack:  # 커서가 맨 뒤가 아니면
