b635e34763b035622fc33312f394c1baa980802f
CreamMeatball <pch5445@gmail.com>
2025-05-29 20:29:29 +0900

    17404 solved

--- 17404.py
+++ 17404.py
@@ -15,28 +15,18 @@ min_cost = float('inf')
 for c in range(3): # 첫번째 집이 c번째 색을 고른 세계관
     for i in range(3):
         if c == i:
-            dp[0][i] = costs[i]
+            dp[0][i] = costs[0][i]
         else: 
-            dp[0][i] = costs[i]
-    for i in range(1, N - 1):
+            dp[0][i] = float('inf')
+    # 첫 값 설정
+            
+    for i in range(1, N):
         dp[i][0] = costs[i][0] + min(dp[i - 1][1], dp[i - 1][2])
         dp[i][1] = costs[i][1] + min(dp[i - 1][0], dp[i - 1][2])
         dp[i][2] = costs[i][2] + min(dp[i - 1][0], dp[i - 1][1])
-    if c == 0:
-        dp[N - 1][0] = float('inf')
-        dp[N - 1][1] = costs[N - 1][1] + min(dp[N - 2][0], dp[N - 2][2])
-        dp[N - 1][2] = costs[N - 1][2] + min(dp[N - 2][0], dp[N - 2][1])
-    elif c == 1:
-        dp[N - 1][0] = costs[N - 1][0] + min(dp[N - 2][1], dp[N - 2][2])
-        dp[N - 1][1] = float('inf')
-        dp[N - 1][2] = costs[N - 1][2] + min(dp[N - 2][0], dp[N - 2][1])
-    elif c == 2:
-        dp[N - 1][0] = costs[N - 1][0] + min(dp[N - 2][1], dp[N - 2][2])
-        dp[N - 1][1] = costs[N - 1][1] + min(dp[N - 2][0], dp[N - 2][2])
-        dp[N - 1][2] = float('inf')
-    print(dp[N - 1])
-    if min(dp[N - 1]) < min_cost:
-        min_cost = min(dp[N - 1])
-        print(min_cost)
+        
+    dp[N - 1][c] = float('inf') # 마지막 집이 첫 집이랑 같은 경우는 불가능이므로.
+        
+    min_cost = min(min_cost, dp[N - 1][0], dp[N - 1][1], dp[N - 1][2])
 
 print(min_cost)
