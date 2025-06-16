2d0d5a47ef50d2447bb87f298450ce6e7b9a83f7
CreamMeatball <pch5445@gmail.com>
2025-06-16 20:57:20 +0900

    1041 solved

--- 1041.py
+++ 1041.py
@@ -5,20 +5,49 @@
 
 N = int(input())
 numbers = list(map(int, input().split()))
-numbers.sort()
+
+def getadjacent(numbers: list, n: int):
+    if n == 1:
+        return numbers
+    elif n == 2:
+        return [
+            sum([numbers[0], numbers[4]]),
+            sum([numbers[0], numbers[3]]),
+            sum([numbers[0], numbers[2]]),
+            sum([numbers[0], numbers[1]]),
+            sum([numbers[3], numbers[4]]),
+            sum([numbers[2], numbers[4]]),
+            sum([numbers[1], numbers[2]]),
+            sum([numbers[1], numbers[3]]),
+            sum([numbers[1], numbers[5]]),
+            sum([numbers[2], numbers[5]]),
+            sum([numbers[3], numbers[5]]),
+            sum([numbers[4], numbers[5]]),
+        ]
+    elif n == 3:
+        return [
+            sum([numbers[0], numbers[1], numbers[2]]),
+            sum([numbers[0], numbers[2], numbers[4]]),
+            sum([numbers[0], numbers[3], numbers[4]]),
+            sum([numbers[0], numbers[1], numbers[3]]),
+            sum([numbers[5], numbers[2], numbers[4]]),
+            sum([numbers[5], numbers[3], numbers[4]]),
+            sum([numbers[5], numbers[1], numbers[3]]),
+            sum([numbers[5], numbers[1], numbers[2]]),
+        ]
+
+minOfAdj3 = min(getadjacent(numbers, 3))
+minOfAdj2 = min(getadjacent(numbers, 2))
+minOfAdj1 = min(getadjacent(numbers, 1))
 
 if N == 1:
-    print(sum(numbers) - numbers[-1])
-elif N == 2: # 모두 3면씩 보임
-    print(sum(numbers[:3]) * 8)
+    print(sum(numbers) - max(numbers))
+elif N == 2: # 3면x4 + 2면x4
+    print(minOfAdj3*4 + minOfAdj2*4)
 else:
-    # N = 3) 3x8 + 2x(4x3) + 1x6
-    # N = 4) 3x8 + 2x(8x3) + 1x(4x6)
-    # N = 5) 3x8 + 2x(12x3) + 1x(9x6)
+    # N = 3) 3면x4 + 2면x(4x1+3x2) + 1면x(1x5+1x4)
+    # N = 4) 3면x4 + 2면x(4x2+3x3) + 1면x(4x5+2x4)
+    # N = 5) 3면x4 + 2면x(4x3+3x4) + 1면x(9x5+3x4)
     # ...
-    # 3면x8 + 2면x(N-2)*4*3 + 1면x(N-2)**2
-    print(
-        sum(numbers[:3]) * 8 + \
-        sum(numbers[:2]) * (N - 2) * 4 * 3 + \
-        sum(numbers[:1]) * (N - 2) ** 2
-    )
+    # 3면x4 + 2면x(4*(N-2)+4*(N-1)) + 1면x((N-2)**2*5+(N-2)*4)
+    print(minOfAdj3*4 + minOfAdj2*(4*(N-2)+4*(N-1)) + minOfAdj1*(5*(N-2)**2+4*(N-2)))
