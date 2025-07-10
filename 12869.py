0605f4ddf48bdd8c295cbc24fc972528c3dd2efe
CreamMeatball <pch5445@gmail.com>
2025-07-10 16:35:34 +0900

    12869 solved

--- 12869.py
+++ 12869.py
@@ -1,15 +1,51 @@
+from collections import deque
+
 N = int(input())
 scvs = list(map(int, input().split()))
 
-# 횟수를 DP의 값으로 해야함
-dp = [[[0] * (scvs[0] + 1) for _ in range(scvs[1] + 1)] for _ in range(scvs[2] + 1)]
-# dp[i][j][k]: 각 scv의 hp가 hp1, hp2, hp3 일 때 모든 scv를 파괴하기 위한 최소 공격 횟수
+attack_case1 = [(9)]
+attack_case2 = [(9,3), (3,9)]
+attack_case3 = [(9,3,1), (9,1,3), (3,9,1), (3,1,9), (1,9,3), (1,3,9)]
 
-destroy_count = 0
 
-attack_case = [(9,3,1), (9,1,3), (3,9,1), (3,1,9), (1,9,3), (1,3,9)]
+dq = deque([scvs + [0]])
+if N == 1:
+    visited = [False] * (scvs[0] + 1)
+elif N == 2:
+    visited = [[False] * (scvs[1] + 1) for _ in range(scvs[0] + 1)]
+else:
+    visited = [[[False] * (scvs[2] + 1) for _ in range(scvs[1] + 1)] for _ in range(scvs[0] + 1)]
+    
+while dq:
+    *scv, count = dq.popleft()
+    # print(f"{scv}, {count}")
+    if N == 1:
+        if visited[scv[0]]:
+            continue
+        visited[scv[0]] = True
+    elif N == 2:
+        if visited[scv[0]][scv[1]]:
+            continue
+        visited[scv[0]][scv[1]] = True
+    else:
+        if visited[scv[0]][scv[1]][scv[2]]:
+            continue
+        visited[scv[0]][scv[1]][scv[2]] = True
 
-for i in range(1, scvs[0] + 1):
-    for j in range(1, scvs[1] + 1):
-        for k in range(1, scvs[2] + 1):
-            None
+    dead_count = 0
+    for sc in scv:
+        if sc <= 0:
+            dead_count += 1
+    if dead_count == N:
+        print(count)
+        break
+        
+    if N == 1:
+        dq.append([max(scv[0] - attack_case1[0], 0), count + 1])
+    elif N == 2:
+        for ac in attack_case2:
+            dq.append([max(scv[0] - ac[0], 0), max(scv[1] - ac[1], 0), count + 1])
+    else:
+        for ac in attack_case3:
+            dq.append([max(scv[0] - ac[0], 0), max(scv[1] - ac[1], 0), max(scv[2] - ac[2], 0), count + 1])
+    
