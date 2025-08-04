c64463e5b2c44503ef2c9fa916704337cf049fb0
CreamMeatball <pch5445@gmail.com>
2025-08-04 20:41:33 +0900

    15683 solved

--- 15683.py
+++ 15683.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 room = []
-cctv_pos = defaultdict(list)
+cctv_list = []
 wall_pos = []
 total_zero = 0
 
@@ -19,7 +19,7 @@ for i in range(N):
         elif val == 6:
             wall_pos.append((i, j))
         else:
-            cctv_pos[val].append((i, j))
+            cctv_list.append((val, (i, j)))
 
 CCTV_DIRECTIONS = {
     1: [[(-1, 0)], [(1, 0)], [(0, -1)], [(0, 1)]],
@@ -30,27 +30,23 @@ CCTV_DIRECTIONS = {
         [(0, 1), (1, 0), (0, -1)], [(1, 0), (0, -1), (-1, 0)]],
     5: [[(-1, 0), (1, 0), (0, -1), (0, 1)]]
 }
-def calculateSee(start: tuple[int, int], direction: tuple[int, int]) -> set[tuple[int, int]]:
+def calculateSee(start, direction):
     """start 위치에서 direction 으로 진행하며 볼 수 있는 (0)칸 좌표 집합 반환"""
-    y, x = start
-    dy, dx = direction
+    r, c = start
+    dr, dc = direction
     seen = set()
     while True:
-        y += dy
-        x += dx
-        # 경계 또는 벽(6) 만나면 종료
-        if not (0 <= y < N and 0 <= x < M) or room[y][x] == 6:
-            break
-        # CCTV는 통과만 하고 시야에 포함시키지 않는다
-        if 1 <= room[y][x] <= 5:
-            continue
-        # 빈 칸이면 시야에 포함
-        seen.add((y, x))
-    return seen
+        r += dr
+        c += dc
 
-cctv_list = []
-for typ in range(1, 6):
-    cctv_list.extend([(typ, pos) for pos in cctv_pos[typ]])
+        if not (0 <= r < N and 0 <= c < M) or room[r][c] == 6:
+            break
+
+        if 1 <= room[r][c] <= 5:
+            continue
+
+        seen.add((r, c))
+    return seen
 
 if not cctv_list:
     print(total_zero)
@@ -58,18 +54,18 @@ if not cctv_list:
 
 coverage_options = []
 
-for typ, (y, x) in cctv_list:
+for typ, (r, c) in cctv_list:
     each = []
-    for dirs in CCTV_DIRECTIONS[typ]:
+    for direction in CCTV_DIRECTIONS[typ]:
         watch = set()
-        for d in dirs:
-            watch |= calculateSee((y, x), d) 
+        for d in direction:
+            watch |= calculateSee((r, c), d) # union set | set
         each.append(watch)
     coverage_options.append(each)
 
 min_blind = total_zero
 
-def dfs(idx: int, watched: set[tuple[int, int]]):
+def dfs(idx, watched):
     global min_blind
     
     if min_blind == 0:
@@ -77,14 +73,50 @@ def dfs(idx: int, watched: set[tuple[int, int]]):
 
     if idx == len(cctv_list):
         blind = total_zero - len(watched)
-        if blind < min_blind:
-            min_blind = blind
+        min_blind = min(min_blind, blind)
         return
 
     for cov in coverage_options[idx]:
+        dfs(idx + 1, watched | cov) # 파라미터에다가 CCTV로 본 영역을 합치기
+        # 일반적인 방식이면 재귀 호출 이후에 backtracking을 위한 별도 처리를 해줘야 되는데, 이렇게 하면 안 해도 됨. 파라미터 상에 그 때의 state가 저장돼있는 거라.
 
-        dfs(idx + 1, watched | cov)
-
-dfs(0, set())
+dfs(0, set()) # 확인한 영역들 합을 파라미터에다 저장함. 백트래킹 하기 용이.
 
 print(min_blind)
+
+# 일반적인 방식 ⬇
+# # 1) 전역 감시 집합
+# watched = set() # 현재까지 감시된 칸을 in-place(한 위치, 제자리에서)로 관리
+# min_blind = total_zero
+
+# def dfs(idx):
+#     global min_blind
+
+#     # 가지치기
+#     if min_blind == 0:
+#         return
+
+#     # 모든 CCTV에 대해 방향을 고르면
+#     if idx == len(cctv_list):
+#         blind = total_zero - len(watched)
+#         if blind < min_blind:
+#             min_blind = blind
+#         return
+
+#     # 3) 이번 CCTV의 모든 회전 경우 시도
+#     for cov in coverage_options[idx]:
+#         # (1) 이번 회전으로 새로 추가된 칸만 기록
+#         newly_added = []
+#         for pos in cov:
+#             if pos not in watched: # 이미 본 칸은 다시 넣을 필요 없음
+#                 watched.add(pos)
+#                 newly_added.append(pos) # 이번 탐색으로 인해 추가되는 애
+
+#         dfs(idx + 1) # (2) 재귀 호출
+
+#         # (3) 백트래킹 : 새로 넣었던 칸만 원상 복구
+#         for pos in newly_added:
+#             watched.remove(pos)
+
+# # 4) 탐색 시작
+# dfs(0)
