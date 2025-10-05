c14dee89c7e75ff7c4732a8ef643af228c255085
Cream_Meatball <80515579+CreamMeatball@users.noreply.github.com>
2025-10-06 01:09:28 +0900

    21921 solved

---
+++ 21921.py
@@ -0,0 +1,22 @@
+import sys
+
+N, X = map(int, input().split())
+visitors = list(map(int, input().split()))
+
+current_sum = sum(visitors[:X])
+max_sum = current_sum
+count = 1
+
+for i in range(X, N):
+    current_sum += visitors[i] - visitors[i - X]  # 윈도우 이동
+    if current_sum > max_sum:
+        max_sum = current_sum
+        count = 1
+    elif current_sum == max_sum:
+        count += 1
+
+if max_sum == 0:
+    print("SAD")
+else:
+    print(max_sum)
+    print(count)
